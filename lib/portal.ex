ab33c3a3e315b9738cbf12cdc853b6959279a04a
betocattani <contato.cattani@gmail.com>
2015-12-20 14:56:36 -0200

    create struct portal, add function transfer

--- lib/portal.ex
+++ lib/portal.ex
@@ -1,4 +1,35 @@
 defmodule Portal do
+
+  defstruct [:left, :right]
+
+  @doc """
+  Starts transfering 'data' from 'left' to 'right'.
+  """
+  def transfer(left, right, data) do
+    # First add all data to the portal on the left
+    for item <- data do
+      Portal.Door.push(left, item)
+    end
+
+    # Returns a portal struck we will use next
+    %Portal{left: left, right: right}
+  end
+
+  @doc """
+  Pushes data to the right in the given 'portal'.
+  """
+  def push_right(portal) do
+    # See if we ca pop data from left. If so, push the
+    # popped data to the right. Otherwise, do nothing.
+    case Portal.Door.pop(portal.left) do
+      :error    -> :ok
+      {:ok, h}  -> Portal.Door.push(portal.right, h)
+    end
+
+    # Let's return the portal itself
+    portal
+  end
+
   use Application
 
   # See http://elixir-lang.org/docs/stable/elixir/Application.html
